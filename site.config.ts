7149397cfb0c425d80ae0555202329273a8f5e8c
linmaobang <linmaobang@gmail.com>
2024-02-02 21:58:25 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -23,6 +23,7 @@ export default siteConfig({
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   youtube: 'marklinreading', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  Bilibili:'https://space.bilibili.com/28916229'
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
