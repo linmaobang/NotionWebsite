f307e5a21fd60db6380f862a51447e61a57dfcbe
linmaobang <linmaobang@gmail.com>
2024-02-02 21:57:32 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -22,7 +22,7 @@ export default siteConfig({
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: 'Mark Lin', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'marklinreading', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
